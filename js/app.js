810040f199866b073cd3c95912f05f8deb361783
jenalp6 <jenalp.18@gmail.com>
2022-06-12 22:54:30 +0300

    last version

--- js/app.js
+++ js/app.js
@@ -12,32 +12,88 @@ let currOperand = "";
 let previousOperand = "";
 let operation = "";
 
+let equalOrPercentPressed = false;
+
 //? Butonları taşıyan container için event tanımlama
 btnContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("num")) {
+    //? Herhangi bir sayi(num) sayiya tiklanildi ise
     appendNumber(e.target.textContent);
     updateDisplay();
   }
   if (e.target.classList.contains("operator")) {
+    //? Herhangi bir operator butonuna (+,-,x,/) tiklanildi ise
     chooseOperator(e.target.textContent);
     updateDisplay();
+  }
+  if (e.target.classList.contains("equal")) {
+    //? Esittir butonuna tiklanildi ise
+    calculate();
+    updateDisplay();
+    equalOrPercentPressed = true;
+  }
+  if (e.target.classList.contains("ac")) {
+    //? AC butonuna tiklanildi ise
+    previousOperand = "";
+    currOperand = "";
+    operation = "";
+    updateDisplay();
+  }
+  if (e.target.classList.contains("pm")) {
+    //? PM butonuna tiklanildi ise
+    if (!currOperand) return;
+    currOperand *= -1;
+    updateDisplay();
+  }
+  if (e.target.classList.contains("percent")) {
+    //? Percent butonuna tiklanildi ise
+    if (!currOperand) return;
+    currOperand = currOperand / 100;
+    updateDisplay();
+    equalOrPercentPressed = true;
   }
 });
 
 const appendNumber = (num) => {
   //? ilk sayı 0 ise geri dön
-  if (!currOperand && num === "0") return;
+  if (currOperand === "0" && num === "0") return;
 
-  //? Şu anki sayı '.' ise ve önceki girilen sayı '.' içeriyorsa geri dön
+  //? Eğer ilk olarak 0 girilmisse ve sonrasinda da . haricinde baska
+  //? bir sayi girilmis ise sadece girilen yeni sayiyi degiskene aktar.
+  //? Orn: 09 => 9 , 03 => 3 , 0.1 => 0.1
+  if (currOperand === "0" && num !== ".") {
+    currOperand = num;
+    return;
+  }
+
+  //? Şu an ki sayı '.' ise ve önceki girilen sayı '.' içeriyorsa geri dön
   if (num === "." && currOperand.includes(".")) return;
+
   if (currOperand.length > 10) return;
+
+  if (equalOrPercentPressed) {
+    currOperand = num;
+    equalOrPercentPressed = false;
+    return;
+  }
+
   //? Girilen sayıları birleştir
   currOperand += num;
 };
 
 const updateDisplay = () => {
+  if (currOperand.toString().length > 11) {
+    currOperand = Number(currOperand).toExponential(3); // toExponential sayı basamağı arttığında exponential (noktadan sonra kaç karakter(3) +9.990e+11 )
+  }
   currDisp.textContent = currOperand;
-  prevDisp.textContent = `${previousOperand}  ${operation}`;
+
+  //! Eger bir sayiya basilmadan operator butonlarina basilirsa
+  //! prevDisp'de operatoru gostermeme
+  if (operation && previousOperand) {
+    prevDisp.textContent = `${previousOperand} ${operation}`;
+  } else {
+    prevDisp.textContent = "";
+  }
 };
 
 const chooseOperator = (op) => {
@@ -69,8 +125,13 @@ const calculate = () => {
       calculation = prev / current;
       break;
     default:
-      break;
+      return;
   }
 
   currOperand = calculation;
+
+  //? Esittir butonuna tiklanildiginda ekranda gozukmemesi icin
+  //? previousOperand ve operation'ı silmemiz gerekir
+  previousOperand = "";
+  operation = "";
 };
